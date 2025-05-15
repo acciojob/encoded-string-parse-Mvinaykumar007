96141b81f135116472e85a5aa9f60fca00ce04ea
Mvinaykumar007 <mprabhakarr21@gmail.com>
2025-05-15 07:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(str) {
+  // Split the string using one or more zeros as separators
+  const [firstName, lastName, id] = str.split(/0+/);
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
+}
+
+// Example usage:
+console.log(parseCode("John000Doe000123"));       // { firstName: "John", lastName: "Doe", id: "123" }
+console.log(parseCode("michael0smith004331"));    // { firstName: "michael", lastName: "smith", id: "4331" }
+console.log(parseCode("Thomas00LEE0000043"));     // { firstName: "Thomas", lastName: "LEE", id: "43" }
